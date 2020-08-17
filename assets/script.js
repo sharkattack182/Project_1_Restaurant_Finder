c03052290dac2aa2d85d933ef2bc1ae312aeeedb
Matthew Courtney <matthewdc182@gmail.com>
2020-08-17 17:03:02 -0400

    got the location working and pulling 3 local restaurants

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,24 @@
 
 $.get("https://ipapi.co/json").then(function (response) {
-	console.log(response);
+    console.log(response);
+    console.log(response.latitude);
+    console.log(response.longitude);
+
+    var latitude = response.latitude;
+    var longitude = response.longitude;
+
+
+
+var settings = {  //cuisine type quert
+    headers:{
+    'user-key': "34f65fbedd34ab6e2274901ccfc95f56"
+    },
+    method: "GET",
+    url: "https://developers.zomato.com/api/v2.1/search?entity_id=%23%23&count=3&lat=" + latitude +"&lon="+ longitude +"&radius=58046.72"
+
+}
+$.ajax(settings).then(function (res) {
+    console.log(res);
+})
+
 });
