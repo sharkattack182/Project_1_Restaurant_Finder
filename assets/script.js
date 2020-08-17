a7cbf09b44bcac728c4b769ee1132e7f4c5217b9
Matthew Courtney <matthewdc182@gmail.com>
2020-08-17 17:26:35 -0400

    added in all the jquery to fill out card 1. Needs to go in a for loop now

--- assets/script.js
+++ assets/script.js
@@ -9,16 +9,22 @@ $.get("https://ipapi.co/json").then(function (response) {
 
 
 
-var settings = {  //cuisine type quert
-    headers:{
-    'user-key': "34f65fbedd34ab6e2274901ccfc95f56"
-    },
-    method: "GET",
-    url: "https://developers.zomato.com/api/v2.1/search?entity_id=%23%23&count=3&lat=" + latitude +"&lon="+ longitude +"&radius=58046.72"
+    var settings = {  //cuisine type quert
+        headers: {
+            'user-key': "34f65fbedd34ab6e2274901ccfc95f56"
+        },
+        method: "GET",
+        url: "https://developers.zomato.com/api/v2.1/search?entity_id=%23%23&count=3&lat=" + latitude + "&lon=" + longitude + "&radius=58046.72"
 
-}
-$.ajax(settings).then(function (res) {
-    console.log(res);
-})
+    }
+    $.ajax(settings).then(function (res) {
+        console.log(res);
+
+        $(".name").text((res.restaurants[0].restaurant.name))
+        $(".description").text((res.restaurants[0].restaurant.cuisines))
+        $(".address").text((res.restaurants[0].restaurant.location.address))
+        $(".phone").text((res.restaurants[0].restaurant.phone_numbers))
+        $(".rating").text(("Rating: " + res.restaurants[0].restaurant.user_rating.aggregate_rating))
+    })
 
 });
